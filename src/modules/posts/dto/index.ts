1cfaaad6464fb6df132c50e96951119386ea5360
Roman <romazelenskiy2000@gmail.com>
2024-08-13 11:43:54 +0300

    add decorators swagger

--- src/modules/posts/dto/index.ts
+++ src/modules/posts/dto/index.ts
@@ -1,15 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString,IsArray } from "class-validator";
 
 export class CreatePostDTO {
+
+    @ApiProperty({ description: 'Title of the post' })
     @IsString()
     title: string;
 
+    @ApiProperty({ description: 'Image URLs' })
     @IsString()
     img: string;
 
+    @ApiProperty({ description: 'Full description of the post' })
     @IsString()
     description: string;
 
+    @ApiProperty({ description: 'Small description of the post' })
     @IsString()
     smallDescription: string;
 }
