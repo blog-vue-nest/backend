7ca4abb8397063d3ecf4765d6ebc67433fcc6e8d
Roman <romazelenskiy2000@gmail.com>
2024-08-13 11:45:12 +0300

    fis decorators swagger

--- src/modules/posts/dto/index.ts
+++ src/modules/posts/dto/index.ts
@@ -3,19 +3,19 @@ import { IsString,IsArray } from "class-validator";
 
 export class CreatePostDTO {
 
-    @ApiProperty({ description: 'Title of the post' })
+    @ApiProperty({ description: 'Title of the post max: 255 characters' })
     @IsString()
     title: string;
 
-    @ApiProperty({ description: 'Image URLs' })
+    @ApiProperty({ description: 'Image URLs max: 255 characters' })
     @IsString()
     img: string;
 
-    @ApiProperty({ description: 'Full description of the post' })
+    @ApiProperty({ description: 'Full description of the post max: 255 characters' })
     @IsString()
     description: string;
 
-    @ApiProperty({ description: 'Small description of the post' })
+    @ApiProperty({ description: 'Small description of the post max: 255 characters' })
     @IsString()
     smallDescription: string;
 }
