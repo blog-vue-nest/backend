999ca97f9094d8246dc000a3b2d0dd1a4908dd8d
Roman <romazelenskiy2000@gmail.com>
2024-08-16 22:23:04 +0300

    fix posts pagination

--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -20,16 +20,27 @@ export class PostsService {
     }
   }
 
-  getAll(page: number): Promise<Post[]> {
+  async getAll(
+    page: number,
+  ): Promise<{ data: Post[]; quantityPages: number; page: number }> {
     try {
       const limitInPage = 9;
       const offset = (page - 1) * limitInPage;
+      const totalPosts = await this.postRepository.count();
 
-      return this.postRepository.findAll({
+      const dataPosts = await this.postRepository.findAll({
         include: [Category],
         limit: limitInPage,
         offset: offset,
       });
+
+      const quantityPages = Math.ceil(totalPosts / limitInPage);
+
+      return {
+        data: dataPosts,
+        quantityPages: quantityPages,
+        page: page,
+      };
     } catch (error: any) {
       throw new Error(error);
     }
