89962cf0c4bbb74b7983a4d6d21cade161700d87
Roman <romazelenskiy2000@gmail.com>
2024-08-24 15:40:37 +0300

    fix upload image post in format base 64

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import type { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
+
+  app.useBodyParser('json', { limit: '100mb' });
 
   app.useGlobalPipes(new ValidationPipe());
 
